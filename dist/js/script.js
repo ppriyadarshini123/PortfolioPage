361c1d23b1f3c0c871427b24f935a7360cb18cb2
payal <payal.priyadarshini@gmail.com>
2020-07-06 23:00:15 +0100

    implemented logos and posters page

--- dist/js/script.js
+++ dist/js/script.js
@@ -89,6 +89,41 @@
         }/*if*/
     }//validation
 
+   /* /!**
+     * @name postersSlider
+     * @desc Sliding all posters on posters.html
+     *!/
+    function postersSlider()
+    {
+        var slideIndex = 1;
+        showSlides(slideIndex);
+
+// Next/previous controls
+        function plusSlides(n) {
+            showSlides(slideIndex += n);
+        }
+
+// Thumbnail image controls
+        function currentSlide(n) {
+            showSlides(slideIndex = n);
+        }
+
+        function showSlides(n) {
+            var i;
+            var slides = document.getElementsByClassName("mySlides");
+            var dots = document.getElementsByClassName("dot");
+            if (n > slides.length) {slideIndex = 1}
+            if (n < 1) {slideIndex = slides.length}
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex-1].style.display = "block";
+            dots[slideIndex-1].className += " active";
+        }
+    }/!*postersSlider*!/*/
 
     /**
      * @name bindBtns
@@ -97,6 +132,7 @@
     function bindBtns() {
         validation();
         myFunction();
+        postersSlider();
     }//bindBtns
 
     /**
